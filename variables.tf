1f5c1598574045a4759f0b9b93ee934476ce6d15
sreev <sreev@SREEVANI>
2026-04-23 15:47:24 +0530

    added

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "location" {
+  default = "East Asia"
+}
+
+variable "resource_group_name" {
+  default = "AppRG"
+}
+
+variable "admin_username" {
+  default = "azadmin"
+}
+
+variable "admin_password" {
+  default = "sreevani@123"
+}
+
+variable "mysql_admin" {
+  default = "azureuser"
+}
+
+variable "mysql_password" {
+  default = "sreevani!123"
+}
+variable "postgre_admin" {
+  default = "pgadmin"
+  
+}
+variable "postgre_password" {
+  default = "sreevani!12345"
+  
+}
